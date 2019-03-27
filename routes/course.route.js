47059b6ca08f4e998513a3473680cbbf6469d253
Ankita <ankita.k1610@gmail.com>
2019-03-27 18:33:37 +0530

    add search

--- routes/course.route.js
+++ routes/course.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 const { Course, User } = require('../sequelize')
 
 router.get('/all/courses', (req, res) => {
@@ -15,7 +16,7 @@ router.get('/details/:id', (req, res) => {
     Course.findOne({
         where: { id: req.params.id }
     }).then(course => {
-        User.findOne({ attributes: ['name', 'email', 'id'], where: { id: course.dataValues.UserId } }).then(author => {
+        User.findOne({ attributes: ['firstName', 'lastName', 'email', 'id'], where: { id: course.dataValues.UserId } }).then(author => {
             return res.json({ course: { course, author } }).status(200)
         }).catch(err => {
             return res.json(err).status(400)
@@ -74,6 +75,20 @@ router.delete('/delete/:id', (req, res) => {
     }).catch(err => {
         return res.json(err).status(400);
     })
+})
+router.get('/search/:query', (req, res) => {
+    course.findAll({
+        where: {
+            name: {
+                [Op.like]: req.params.query + '%'
+            }
+        },
+        order: ['course_name']
+    }).then(courses => {
+        return res.json(courses).status(200)
+    }).catch(err => {
+        return res.json(err).status(400)
+    });
 })
 
 module.exports = router
