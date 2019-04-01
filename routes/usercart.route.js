f0f9f8524340b42bec6f9883d5e965e02d652a71
Ankita <ankita.k1610@gmail.com>
2019-04-01 18:26:15 +0530

    complete cart implementation - 1/4/19

--- routes/usercart.route.js
+++ routes/usercart.route.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const { UserCart } = require('../sequelize')
+const { UserCart, Course } = require('../sequelize')
 
 router.get('/user/:userId', (req, res) => {
-    UserCart.findAndCountAll({ where: { UserId: req.params.userId } }).then(result => {
-        return res.json(result).status(200)
+    UserCart.findAll({ where: { UserId: req.params.userId } }).then(async cart => {
+        let courseData = []
+        await Promise.all(cart.map(async c => {
+            let id = Number(c.dataValues.CourseId)
+            await Course.findOne({ where: { id } }).then(course => {
+                courseData.push(course.dataValues)
+            })
+        }))
+        return res.json({ cart, courseData }).status(200)
     }).catch(err => {
         return res.json(err).status(400)
     })
@@ -22,6 +29,14 @@ router.post('/user/new', (req, res) => {
 router.delete('/user/:userId/course/:courseId', (req, res) => {
     UserCart.destroy({ where: { CourseId: req.params.courseId, UserId: req.params.userId } }).then(resp => {
         return res.json(resp).status(200)
+    }).catch(err => {
+        return res.json(err).status(400)
+    })
+})
+
+router.get('/count/:userId', (req, res) => {
+    UserCart.count({ where: { UserId: req.params.userId } }).then(count => {
+        return res.json(count).status(200)
     }).catch(err => {
         return res.json(err).status(400)
     })
